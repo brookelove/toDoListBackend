0c39265661fd68fae3bc3d2cb5dbd800283b9712
Brooke Love <savienloveb@gmail.com>
2022-08-25 13:56:12 -0700

    adding notes

--- models/Calendar.js
+++ models/Calendar.js
@@ -31,6 +31,7 @@ Calender.init(
       allowNull: false,
       //   might need to create a custom validaor  for the location but can do that on the react side
       //   isLocation:
+      //   validate would be like: a string of numbers between 3 and 6 followed by a space and then another section of combo of numbers letters and spaces followed by a comma then a combination of letters either lowercase or uppercase that are between 3-17 followed by a comma and there is either 2 uppercase letters or a combo of letters that are upper and lower between 5 and twelve it can have a space followed by a comma and 5 numbers
     },
   },
   {
