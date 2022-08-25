6baaedc7cb6c9209555e26771e6857d1b634595c
Brooke Love <savienloveb@gmail.com>
2022-08-24 20:05:00 -0700

    adding basic code for server connection and database

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS the_blog_db;
+CREATE DATABASE the_blog_db;sche
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const allRoutes = require("./controllers");
+const session = require("express-session");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    //this will give you two hours
+    maxAge: 2 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+// Static directory
+app.use(express.static("public"));
+
+// need to ake the app work for react
+// const hbs = exphbs.create({});
+// app.engine('', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+app.use("/", allRoutes);
+
+sequelize.sync({ force: false }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
