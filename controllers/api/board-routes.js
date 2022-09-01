746598394905edd6781bc09f879cc4ca2f9a0335
Brooke Love <brookelove@Brookes-iMac-2.local>
2022-09-01 13:29:32 -0700

    creating route for blog

--- controllers/api/board-routes.js
+++ controllers/api/board-routes.js
@@ -1,4 +1,4 @@
 // neeed to be able to do all CRUD opperations for the board notes
 const express = require("express");
 const router = express.Router();
-const { Board } = require("../../models");
+const { Board, Calender } = require("../../models");
---
+++ controllers/api/calender-routes.js
@@ -0,0 +1,4 @@
+// neeed to be able to do all CRUD opperations for the board notes
+const express = require("express");
+const router = express.Router();
+const { Calender } = require("../../models");
